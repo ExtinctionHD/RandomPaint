4b56165ab130c6fe525c55caee81548332566c4a
ExtinctionHD <Extinction132@gmail.com>
2018-04-22 13:25:22 +0300

    Update RandomCreator, now it using CreateInstance method

--- Paint/classes/Creator.cs
+++ Paint/classes/Creator.cs
@@ -96,40 +96,11 @@ namespace Paint.classes
     {
         public override Shape FactoryMethod(Color color, Point v1, Point v2)
         {
-            Type ourtype = typeof(Creator);
-            int count = Assembly.GetAssembly(ourtype).GetTypes().Where(type => type.IsSubclassOf(ourtype)).Count() - 1;
+            Type shape = typeof(Shape);
+            List<Type> shapeTypes = Assembly.GetAssembly(shape).GetTypes().Where(type => type.IsSubclassOf(shape) && !type.IsAbstract).ToList();
+            int i = new Random().Next(shapeTypes.Count);
 
-            int i = new Random().Next(count);
-
-            switch (i)
-            {
-                case 0:
-                    return new Rectangle(color, v1, v2);
-
-                case 1:
-                    return new Square(color, v1, v2);
-
-                case 2:
-                    return new Ellipse(color, v1, v2);
-
-                case 3:
-                    return new Circle(color, v1, v2);
-
-                case 4:
-                    return new IsoscelesTriangle(color, v1, v2);
-
-                case 5:
-                    return new RightTriangle(color, v1, v2);
-
-                case 6:
-                    return new Pentagon(color, v1, v2);
-
-                case 7:
-                    return new Hexagon(color, v1, v2);
-
-                default:
-                    return new Star(color, v1, v2);
-            }
+            return (Shape)Activator.CreateInstance(shapeTypes[i], new Object[] { color, v1, v2 });
         }
     }
 }
